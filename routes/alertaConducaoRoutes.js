b84a734691e91828053d1b30bab2e46ebcaa0309
joaothallys <joaothallys>
2024-06-18 16:08:21 -0300

    corrigingo alerta de conducao

--- routes/alertaConducaoRoutes.js
+++ routes/alertaConducaoRoutes.js
@@ -1,85 +1,85 @@
 const express = require('express');
 const router = express.Router();
 
-// Função que define as rotas para Historico_Conducao
-function historicoConducaoRoutes(db) {
+// Função que define as rotas para Alerta_Conducao
+function alertaConducaoRoutes(db) {
 
-    // Rota para criar um novo registro no histórico de condução
-    router.post('/historico-conducao', (req, res) => {
-        const { id_motorista, id_veiculo, data_historico_conducao, disponibilidade, status_saude, pronto_para_viagem, nome_completo } = req.body;
+    // Rota para criar um novo alerta de condução
+    router.post('/alerta-conducao', (req, res) => {
+        const { tipo_alerta_conducao, descricao_alerta_conducao, gravidade_alerta, tratativa_alerta, data_alerta_conducao, id_motorista, id_veiculo, id_viagem } = req.body;
 
-        if (!id_motorista || !id_veiculo || !data_historico_conducao) {
+        if (!tipo_alerta_conducao || !gravidade_alerta || !tratativa_alerta) {
             return res.status(400).send('Erro: Todos os campos obrigatórios devem ser fornecidos.');
         }
 
-        db.query('INSERT INTO Historico_Conducao (id_motorista, id_veiculo, data_historico_conducao, disponibilidade, status_saude, pronto_para_viagem, nome_completo) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [id_motorista, id_veiculo, data_historico_conducao, disponibilidade, status_saude, pronto_para_viagem, nome_completo],
+        db.query('INSERT INTO Alerta_Conducao (tipo_alerta_conducao, descricao_alerta_conducao, gravidade_alerta, tratativa_alerta, data_alerta_conducao, id_motorista, id_veiculo, id_viagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [tipo_alerta_conducao, descricao_alerta_conducao, gravidade_alerta, tratativa_alerta, data_alerta_conducao, id_motorista, id_veiculo, id_viagem],
             (err, result) => {
                 if (err) {
                     console.error(err);
-                    return res.status(500).send('Erro ao criar registro no histórico de condução');
+                    return res.status(500).send('Erro ao criar alerta de condução');
                 }
-                res.send('Registro criado com sucesso');
+                res.send('Alerta criado com sucesso');
             });
     });
 
-    // Rota para buscar um registro por ID
-    router.get('/historico-conducao/:id', (req, res) => {
-        const id_historico_conducao = req.params.id;
+    // Rota para buscar um alerta por ID
+    router.get('/alerta-conducao/:id', (req, res) => {
+        const id_alerta_conducao = req.params.id;
 
-        db.query('SELECT * FROM Historico_Conducao WHERE id_historico_conducao = ?', [id_historico_conducao], (err, results) => {
+        db.query('SELECT * FROM Alerta_Conducao WHERE id_alerta_conducao = ?', [id_alerta_conducao], (err, results) => {
             if (err) {
                 console.error(err);
-                return res.status(500).send('Erro ao buscar registro no histórico de condução');
+                return res.status(500).send('Erro ao buscar alerta de condução');
             }
             if (results.length === 0) {
-                return res.status(404).send('Registro não encontrado');
+                return res.status(404).send('Alerta não encontrado');
             }
             res.json(results[0]);
         });
     });
 
-    // Rota para buscar todos os registros
-    router.get('/historico-conducao', (req, res) => {
-        db.query('SELECT * FROM Historico_Conducao', (err, results) => {
+    // Rota para buscar todos os alertas
+    router.get('/alerta-conducao', (req, res) => {
+        db.query('SELECT * FROM Alerta_Conducao', (err, results) => {
             if (err) {
                 console.error(err);
-                return res.status(500).send('Erro ao buscar registros do histórico de condução');
+                return res.status(500).send('Erro ao buscar alertas de condução');
             }
             res.json(results);
         });
     });
 
-    // Rota para atualizar um registro existente
-    router.put('/historico-conducao/:id', (req, res) => {
-        const id_historico_conducao = req.params.id;
-        const { id_motorista, id_veiculo, data_historico_conducao, disponibilidade, status_saude, pronto_para_viagem, nome_completo } = req.body;
+    // Rota para atualizar um alerta existente
+    router.put('/alerta-conducao/:id', (req, res) => {
+        const id_alerta_conducao = req.params.id;
+        const { tipo_alerta_conducao, descricao_alerta_conducao, gravidade_alerta, tratativa_alerta, data_alerta_conducao, id_motorista, id_veiculo, id_viagem } = req.body;
 
-        db.query('UPDATE Historico_Conducao SET id_motorista = ?, id_veiculo = ?, data_historico_conducao = ?, disponibilidade = ?, status_saude = ?, pronto_para_viagem = ?, nome_completo = ? WHERE id_historico_conducao = ?',
-            [id_motorista, id_veiculo, data_historico_conducao, disponibilidade, status_saude, pronto_para_viagem, nome_completo, id_historico_conducao],
+        db.query('UPDATE Alerta_Conducao SET tipo_alerta_conducao = ?, descricao_alerta_conducao = ?, gravidade_alerta = ?, tratativa_alerta = ?, data_alerta_conducao = ?, id_motorista = ?, id_veiculo = ?, id_viagem = ? WHERE id_alerta_conducao = ?',
+            [tipo_alerta_conducao, descricao_alerta_conducao, gravidade_alerta, tratativa_alerta, data_alerta_conducao, id_motorista, id_veiculo, id_viagem, id_alerta_conducao],
             (err, result) => {
                 if (err) {
                     console.error(err);
-                    return res.status(500).send('Erro ao atualizar registro no histórico de condução');
+                    return res.status(500).send('Erro ao atualizar alerta de condução');
                 }
-                res.send('Registro atualizado com sucesso');
+                res.send('Alerta atualizado com sucesso');
             });
     });
 
-    // Rota para excluir um registro
-    router.delete('/historico-conducao/:id', (req, res) => {
-        const id_historico_conducao = req.params.id;
+    // Rota para excluir um alerta
+    router.delete('/alerta-conducao/:id', (req, res) => {
+        const id_alerta_conducao = req.params.id;
 
-        db.query('DELETE FROM Historico_Conducao WHERE id_historico_conducao = ?', [id_historico_conducao], (err, result) => {
+        db.query('DELETE FROM Alerta_Conducao WHERE id_alerta_conducao = ?', [id_alerta_conducao], (err, result) => {
             if (err) {
                 console.error(err);
-                return res.status(500).send('Erro ao excluir registro no histórico de condução');
+                return res.status(500).send('Erro ao excluir alerta de condução');
             }
-            res.send('Registro excluído com sucesso');
+            res.send('Alerta excluído com sucesso');
         });
     });
 
     return router;
 }
 
-module.exports = historicoConducaoRoutes;
+module.exports = alertaConducaoRoutes;
